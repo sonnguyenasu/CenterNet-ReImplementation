abafedbafaf1a7b3deadf3aadda2b7f94a02a17c
Sơn Nguyễn <tson.csp@gmail.com>
2021-02-26 11:39:47 +0700

    Update base.py

--- data/base.py
+++ data/base.py
@@ -12,7 +12,27 @@ class Base(Dataset):
         self.images = [os.path.join(image_path, img)
                        for img in os.listdir(image_path)]
         self.num_class = num_class
+    def gaussian_radius(self,det_size, min_overlap=0.7):
+        height, width = det_size
 
+        a1  = 1
+        b1  = (height + width)
+        c1  = width * height * (1 - min_overlap) / (1 + min_overlap)
+        sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
+        r1  = (b1 + sq1) / 2
+
+        a2  = 4
+        b2  = 2 * (height + width)
+        c2  = (1 - min_overlap) * width * height
+        sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
+        r2  = (b2 + sq2) / 2
+
+        a3  = 4 * min_overlap
+        b3  = -2 * min_overlap * (height + width)
+        c3  = (min_overlap - 1) * width * height
+        sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
+        r3  = (b3 + sq3) / 2
+        return min(r1, r2, r3)
     def _convert_box_to_center_mask(self, boxes, height, width):
         return None
 
@@ -41,7 +61,7 @@ class CustomDataset(Base):
         return center
 
     def _convert_box_to_center_mask(self, boxes, height, width):
-        center_mask = np.zeros((height//4, width//4, self.num_class))
+        center_mask = self.gaussian_radius((height//4,width//4))/3#np.zeros((height//4, width//4, self.num_class))
         intended_value = np.zeros((height//4, width//4))
 
         x = np.arange(width//4)
