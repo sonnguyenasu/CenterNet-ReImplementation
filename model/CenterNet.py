97b16e6274b53705d6dd865889cad2fbb7cb838a
Sơn Nguyễn <tson.csp@gmail.com>
2021-02-27 17:41:26 +0700

    Update CenterNet.py

--- model/CenterNet.py
+++ model/CenterNet.py
@@ -13,7 +13,10 @@ class CenterNet(nn.Module):
             self.backbone = ResNet()
         self.fpn = FPN(in_blocks=blocks)
         self.prediction = nn.Conv2d(256, num_class+4, 1)
-        self.center_feed = nn.Sigmoid()
+        self.center_feed = nn.Conv2d(num_class,num_class,1)
+        self.size_feed = nn.Conv2d(2,2,1)
+        self.center_feed.bias.data.fill_(-2.19)
+        self.size_feed.bias.data.fill_(5.8)
         # self.offset_feed = nn.Conv2d(2, 2, 1)
         # self.size_feed = nn.Conv2d(2, 2, 1)
 
@@ -23,9 +26,9 @@ class CenterNet(nn.Module):
         prediction = self.prediction(feed)
         center_predict, offset_predict, size_predict = torch.split(
             prediction, [self.num_class, 2, 2], 1)
-        center_predict = self.center_feed(center_predict)
-        # offset_predict = self.offset_feed(offset_predict)
-        # size_predict = self.size_feed(size_predict)
+        center_predict = torch.sigmoid(self.center_feed(center_predict))
+        offset_predict = torch.exp(offset_predict)
+        size_predict = torch.exp(self.size_feed(size_predict))
         prediction = torch.cat(
             [center_predict, offset_predict, size_predict], dim=1)
         return prediction
