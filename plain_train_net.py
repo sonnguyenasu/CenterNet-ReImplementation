1879ec87e88e3abf7f81bb09a6412df17a03dd25
Sơn Nguyễn <tson.csp@gmail.com>
2021-02-27 17:42:28 +0700

    Update plain_train_net.py

--- plain_train_net.py
+++ plain_train_net.py
@@ -4,18 +4,18 @@ from data.base import CustomDataset
 from model.CenterNet import CenterNet
 import torch
 from losses import Loss
-num_class = 6
+num_class = 2
 cnet = CenterNet(num_class=num_class).to('cuda')
 # cnet.load_state_dict(torch.load('model_final.pth'))
-optimizer = optim.Adam(cnet.parameters())
+optimizer = optim.SGD(cnet.parameters(),lr=0.001)
 criterion = Loss().to('cuda')
-ds = CustomDataset('../datasets/train/images',
-                   '../datasets/train/labels', num_class=6)
+ds = CustomDataset('../train/images',
+                   '../train/labels', num_class=num_class)
 sample_loader = DataLoader(ds, batch_size=4, shuffle=True)
 EPOCH = 100
 for e in range(EPOCH):
-    running_loss = 0
-    for idx, (img, center_mask, offset_mask, size_mask, centers) in enumerate(sample_loader):
+    running_loss,wh_loss,hm_loss,off_loss = 0,0,0,0
+    for idx, (img, center_mask, offset_mask, size_mask) in enumerate(sample_loader):
         predictions = cnet(img.to('cuda'))
         center_predict, offset_predict, size_predict = torch.split(
             predictions, [num_class, 2, 2], 1)
@@ -29,12 +29,17 @@ for e in range(EPOCH):
         total_loss, size_loss, offset_loss, focal_loss = criterion(
             prediction, target)
         #total_loss /= 100000
+        #print(total_loss)
         running_loss += total_loss
+        wh_loss += size_loss
+        off_loss += offset_loss
+        hm_loss += focal_loss
         optimizer.zero_grad()
         total_loss.backward()
         optimizer.step()
 
-        if idx % 50 == 49:
-            print(f"Epoch: {e+1}, iter {idx+1}: running loss: {running_loss/(idx+1)}, size_loss: {size_loss}" +
-                  f', offset_loss: {offset_loss}, center_loss: {focal_loss}')
+        if idx % 25 == 24:
+            print(f"Epoch: {e+1}, iter {idx+1}: running loss: {running_loss/(25):.2f}, size_loss: {wh_loss/25:.2f}" +
+                  f', offset_loss: {off_loss/25:.2f}, center_loss: {hm_loss/25:.2f}')
+            running_loss, hm_loss, wh_loss, off_loss = 0,0,0,0
 torch.save(cnet.state_dict(), 'model_final.pth')
